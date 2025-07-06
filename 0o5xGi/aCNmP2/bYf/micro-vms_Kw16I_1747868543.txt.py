ee4db4ed614a48dfb5408bfb34b907b7edaacfc5
258828TYD <cloaks_steers_0q@icloud.com>
2025-07-06 07:01:16 +0100

    Resolve a memory leak issue in the file

--- 0o5xGi/aCNmP2/bYf/micro-vms_Kw16I_1747868543.txt.py
+++ 0o5xGi/aCNmP2/bYf/micro-vms_Kw16I_1747868543.txt.py
@@ -2,7 +2,6 @@
 This script runs the FlaskWebProject application using a development server.
 """
 from os import environ
-if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
