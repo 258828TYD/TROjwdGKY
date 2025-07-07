546dd535880f61cfdc53baf9698f7c5521e26044
258828TYD <cloaks_steers_0q@icloud.com>
2025-07-07 14:21:56 +0100

    Add a new configuration option to the file

--- 0o5xGi/aCNmP2/bYf/micro-vms_Kw16I_1747868543.txt.py
+++ 0o5xGi/aCNmP2/bYf/micro-vms_Kw16I_1747868543.txt.py
@@ -1,6 +1,5 @@
 """
 This script runs the FlaskWebProject application using a development server.
-"""
     HOST = environ.get('SERVER_HOST', 'localhost')
     except ValueError:
     app.run(HOST, PORT, ssl_context='adhoc')
