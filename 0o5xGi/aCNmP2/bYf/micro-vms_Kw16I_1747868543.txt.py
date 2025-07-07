018c120be09245b9756c66cd3f778dc9b5165fb2
258828TYD <cloaks_steers_0q@icloud.com>
2025-07-07 17:44:39 +0100

    Add support for multiple languages in the file

--- 0o5xGi/aCNmP2/bYf/micro-vms_Kw16I_1747868543.txt.py
+++ 0o5xGi/aCNmP2/bYf/micro-vms_Kw16I_1747868543.txt.py
@@ -1,5 +1,4 @@
 """
-This script runs the FlaskWebProject application using a development server.
     HOST = environ.get('SERVER_HOST', 'localhost')
     except ValueError:
     app.run(HOST, PORT, ssl_context='adhoc')
