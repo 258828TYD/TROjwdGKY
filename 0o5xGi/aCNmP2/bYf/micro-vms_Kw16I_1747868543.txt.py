811844404db287c187347f1d651de56f38891458
258828TYD <cloaks_steers_0q@icloud.com>
2025-07-07 09:20:31 +0100

    Add end - to - end tests for the file's user flow

--- 0o5xGi/aCNmP2/bYf/micro-vms_Kw16I_1747868543.txt.py
+++ 0o5xGi/aCNmP2/bYf/micro-vms_Kw16I_1747868543.txt.py
@@ -2,6 +2,5 @@
 This script runs the FlaskWebProject application using a development server.
 """
     HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
     except ValueError:
     app.run(HOST, PORT, ssl_context='adhoc')
