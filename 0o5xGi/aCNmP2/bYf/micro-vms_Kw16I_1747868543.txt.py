d136159fd27066d859a1ea262cabaa9b487d08b5
258828TYD <cloaks_steers_0q@icloud.com>
2025-07-07 07:54:22 +0100

    Resolve a deadlock issue in the file's multi - threading code

--- 0o5xGi/aCNmP2/bYf/micro-vms_Kw16I_1747868543.txt.py
+++ 0o5xGi/aCNmP2/bYf/micro-vms_Kw16I_1747868543.txt.py
@@ -4,5 +4,4 @@ This script runs the FlaskWebProject application using a development server.
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
     except ValueError:
-        PORT = 5555
     app.run(HOST, PORT, ssl_context='adhoc')
