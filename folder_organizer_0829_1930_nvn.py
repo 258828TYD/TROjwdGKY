c7ed4863ad50060c4d64b21a02fe902e3f44e352
258828TYD <cloaks_steers_0q@icloud.com>
2025-08-29 12:30:05 +0100

    Configure 文件夹结构整理器 using STARLETTE

---
+++ folder_organizer_0829_1930_nvn.py
@@ -0,0 +1,9 @@
+# 代码生成时间: 2025-08-29 19:30:03
+import os
+import shutil
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+from typing import Dict
+
+"
