e1fe24a84f0d87c0cc6514bc2d13e76c24c0ee9f
258828TYD <cloaks_steers_0q@icloud.com>
2025-10-09 20:26:30 +0100

    Add 聚类分析工具 using STARLETTE

---
+++ cluster_analysis_service_1010_0326_zaq.py
@@ -0,0 +1,10 @@
+# 代码生成时间: 2025-10-10 03:26:28
+import numpy as np
+from sklearn.cluster import KMeans
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+from starlette.requests import Request
+from typing import Any, List, Dict
+
+"
